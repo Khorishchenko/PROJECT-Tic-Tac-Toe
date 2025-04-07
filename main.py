8355704b5f936f35c6710bf3cf121fb509eb94dc
admin <admin@MacBook-Air-admin.local>
2025-04-07 11:34:39 +0300

    1.0

---
+++ main.py
@@ -0,0 +1,91 @@
+from random import randrange
+
+def display_board(board):
+        separator = '-' * 25
+        print('+-------+-------+-------+')
+        for i in range(3):
+            print('|       |       |       |')
+            print(f'|   {board[i][0]}   |   {board[i][1]}   |   {board[i][2]}   |')
+            print('|       |       |       |')
+            print('+-------+-------+-------+')
+        print(separator)
+
+
+
+def enter_move(board):
+    # Функція приймає поточний статус дошки, запитує користувача про його хід,
+    # перевіряє введення та оновлює дошку відповідно до рішення користувача.
+    value = input("Введіть свій хід: ")
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == value:
+                board[i][j] = 'O'
+    display_board(board)
+
+
+def make_list_of_free_fields(board):
+    # Функція перевіряє дошку та створює список усіх вільних квадратів;
+    # список складається з кортежів, так що кожен кортеж є парою номерів рядка і стовпчика.
+    free_fields = []
+    valid_values = {'1', '2', '3', '4', '6', '7', '8', '9'}
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] in valid_values:
+                free_fields.append((i, j))
+    return free_fields
+
+
+def winner_for(board, sign):
+    # Функція аналізує стан дошки, щоб перевірити, чи
+    # э в грі переможець
+   # Проверка рядов и столбцов
+    for i in range(3):
+        if all(board[i][j] == sign for j in range(3)) or all(board[j][i] == sign for j in range(3)):
+            return True
+    # Проверка диагоналей
+    if (board[0][0] == sign and board[1][1] == sign and board[2][2] == sign) or \
+       (board[0][2] == sign and board[1][1] == sign and board[2][0] == sign):
+        return True
+    return False
+
+
+def draw_move(board):
+    # Функція малює хід комп'ютера та оновлює дошку.
+    free_fields = make_list_of_free_fields(board)
+    if len(free_fields) > 0:
+        x, y = free_fields[randrange(len(free_fields))]
+        board[x][y] = 'X'
+    else:
+        print('No possible moves left. It\'s a draw!')
+
+
+
+
+def main():
+    # Основна функція для гри
+    board = [['1', '2', '3'],
+            ['4', 'X', '6'],
+            ['7', '8', '9']]
+    print("Welcome to Tic Tac Toe!")
+    display_board(board)
+    while True:
+        # Цикл для гри
+        if len(make_list_of_free_fields(board)) != 0:
+            enter_move(board)
+        else:
+            print('It\'s a draw!')
+            break
+        if winner_for(board, 'X'):
+            print('Player X wins!')
+            break
+        if winner_for(board, 'O'):
+            print('Player O wins!')
+            break
+            
+        draw_move(board)
+        display_board(board)
+
+
+
+if __name__ == "__main__":
+    main()
